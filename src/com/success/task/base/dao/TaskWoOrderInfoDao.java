8e8b41356e470f2b27bc323fd1998fd08b53ee2f
1009568392@qq.com <lzf198733>
2018-06-04 03:49:00 +0800

    用户功能修改

--- src/com/success/task/base/dao/TaskWoOrderInfoDao.java
+++ src/com/success/task/base/dao/TaskWoOrderInfoDao.java
@@ -6,22 +6,46 @@ import com.success.task.base.domain.TaskWoOrderInfo;
 import com.success.web.framework.exception.DaoException;
 import com.success.web.framework.mybatis.BaseDao;
 
+import java.util.List;
+
+/**
+ * 操作工单表
+ */
 @Repository("taskWoOrderInfoDao")
 public class TaskWoOrderInfoDao extends BaseDao {
 
 	/**
 	 * 更新TaskWoOrderInfo数据
-	 * @param taskWoOrderInfo
-	 * @return
+	 * @param taskWoOrderInfo 工单信息
+	 * @return 返回结果
+	 * @throws DaoException 异常
 	 */
-	public Integer insertTaskWoOrderInfo(TaskWoOrderInfo taskWoOrderInfo) throws DaoException{
-		if(taskWoOrderInfo == null) return null;
+	public Integer insertTaskWoOrderInfo(TaskWoOrderInfo taskWoOrderInfo) throws DaoException {
+		if (taskWoOrderInfo == null) {
+			return null;
+		}
 		return this.sqlSession.update("taskWoOrderInfo.insertTaskWoOrderInfo", taskWoOrderInfo);
 	}
-	
-	public Integer updateTaskWoOrderInfo(TaskWoOrderInfo taskWoOrderInfo) throws DaoException{
-		if(taskWoOrderInfo == null) return null;
+
+	/**
+	 * 更新工单信息
+	 * @param taskWoOrderInfo 工单信息
+	 * @return 返回结果
+	 * @throws DaoException 异常
+	 */
+	public Integer updateTaskWoOrderInfo(TaskWoOrderInfo taskWoOrderInfo) throws DaoException {
+		if (taskWoOrderInfo == null) {
+			return null;
+		}
 		return this.sqlSession.update("taskWoOrderInfo.updateTaskWoOrderInfo", taskWoOrderInfo);
 	}
-	
+
+	/**
+	 * 获取订单的工单列表信息
+	 * @param orderId 订单ID
+	 * @return 返回结果
+	 */
+	public List<TaskWoOrderInfo> selectTaskWoOrderInfoListByOrderId(Integer orderId) {
+		return this.sqlSession.selectList("taskWoOrderInfo.selectTaskWoOrderInfoListByOrderId", orderId);
+	}
 }
--- src/com/success/task/detail/service/serviceImpl/TaskWoOrderDpcoiServiceImpl.java
+++ src/com/success/task/detail/service/serviceImpl/TaskWoOrderDpcoiServiceImpl.java
@@ -1,7 +1,9 @@
 package com.success.task.detail.service.serviceImpl;
 
 import com.success.sys.user.domain.User;
+import com.success.task.base.dao.TaskWoOrderInfoDao;
 import com.success.task.base.dao.TaskWoOrderSysInfoDao;
+import com.success.task.base.domain.TaskWoOrderInfo;
 import com.success.task.base.domain.TaskWoOrderSysInfo;
 import com.success.task.base.query.TaskWoOrderDetailQuery;
 import com.success.task.detail.domain.TaskWoOrder;
@@ -26,6 +28,9 @@ public class TaskWoOrderDpcoiServiceImpl implements TaskWoOrderDpcoiService {
     @Resource(name = "taskWoOrderService")
     private TaskWoOrderService taskWoOrderService;
 
+    @Resource(name = "taskWoOrderInfoDao")
+    private TaskWoOrderInfoDao taskWoOrderInfoDao;
+
     @Override
     public void complateTaskWoOrder(Integer orderId, String woOrderInfoValue, User user) throws Exception {
         TaskWoOrderDetailQuery taskWoOrderDetailQuery = new TaskWoOrderDetailQuery();
@@ -34,8 +39,22 @@ public class TaskWoOrderDpcoiServiceImpl implements TaskWoOrderDpcoiService {
         TaskWoOrder taskWoOrder = null;
         for (TaskWoOrder taskWoOrderTemp: taskWoOrderList){
             Integer tacheId = taskWoOrderTemp.getTacheId();
-            if (tacheId == 64) {
+            String woOrderStateCode = taskWoOrderTemp.getWoOrderStateCode();
+            if (tacheId == 64 && "10B".equals(woOrderStateCode)) {
                 taskWoOrder = taskWoOrderTemp;
+            }
+        }
+
+        if (taskWoOrder == null) {
+            return;
+        }
+
+        List<TaskWoOrderInfo> taskWoOrderInfoList = this.taskWoOrderInfoDao.selectTaskWoOrderInfoListByOrderId(orderId);
+        for (TaskWoOrderInfo taskWoOrderInfo : taskWoOrderInfoList) {
+            Integer woOrderId = taskWoOrderInfo.getWoOrderId();
+            if (woOrderId.intValue() == taskWoOrder.getWoOrderId()){
+                taskWoOrderInfo.setWoInfoValue(woOrderInfoValue);
+                this.taskWoOrderInfoDao.updateTaskWoOrderInfo(taskWoOrderInfo);
             }
         }
 
